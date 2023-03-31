231072eba2e268aa4bb4585149336c06bc16b2d6
João Henrique <125095098+RochaJoaos@users.noreply.github.com>
2023-03-31 19:06:32 -0300

    menu update

--- script/index.js
+++ script/index.js
@@ -1,12 +1,15 @@
-var menu = document.querySelector("header");
+function scrollMenu(){
+    var menu = document.querySelector(`header`)
 
-function menuScroll(){
-
+    if(scrollY > 0){
+        menu.style.backgroundColor = 'white';
+        menu.style.boxShadow = ' 1px 0px 1px black';
+    }
+    else{
+        menu.style.backgroundColor = null;   
+        menu.style.boxShadow = null;
+    }
 }
 
-window.addEventListener('scroll', function(){
-    if( window.scrollY > 2 ){
+window.addEventListener('scroll', scrollMenu);
 
-    } 
-    else menu.classList.remove(`menuFixo`)
-})
