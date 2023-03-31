aa720821ea1fffeea1eb84706235dcdd673398f1
João Henrique <125095098+RochaJoaos@users.noreply.github.com>
2023-03-31 16:30:55 -0300

    update

---
+++ script/index.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector("header");
+
+function menuScroll(){
+
+}
+
+window.addEventListener('scroll', function(){
+    if( window.scrollY > 2 ){
+
+    } 
+    else menu.classList.remove(`menuFixo`)
+})
